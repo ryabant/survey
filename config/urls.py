38ec50ddce60c0d4ab5c1ba034eb2f6686bf207d
Anton <ryabant@gmail.com>
2021-07-26 19:38:49 +0300

    add docs

--- config/urls.py
+++ config/urls.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+from django.conf.urls import url
+
 from surveys.views import AnswerViewSet, ChoiceViewSet, QuestionViewSet, SurveyViewSet
 
 router = DefaultRouter()
@@ -13,4 +19,32 @@ router.register(r"answers", AnswerViewSet)
 urlpatterns = [
     path("api/", include(router.urls)),
     path("admin/", admin.site.urls),
+]
+
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Survey API",
+        default_version="v1",
+        description="Docs for survey API",
+        license=openapi.License(name="MIT License"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
+
+urlpatterns += [
+    url(
+        r"^swagger(?P<format>\.json|\.yaml)$",
+        schema_view.without_ui(cache_timeout=0),
+        name="schema-json",
+    ),
+    url(
+        r"^swagger/$",
+        schema_view.with_ui("swagger", cache_timeout=0),
+        name="schema-swagger-ui",
+    ),
+    url(
+        r"^redoc/$", schema_view.with_ui("redoc", cache_timeout=0), name="schema-redoc"
+    ),
 ]
--- surveys/models.py
+++ surveys/models.py
@@ -4,7 +4,8 @@ from django.db import models
 class Survey(models.Model):
     title = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    create_at = models.DateTimeField(auto_now_add=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
 
     def __str__(self):
         return self.title
